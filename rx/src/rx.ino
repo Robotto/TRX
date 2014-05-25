6717e9b5be7d068ceb2ea20adc28bb0e547af353
Robotto <mark@moore.dk>
2014-05-25 15:42:14 +0200

    added BEEP and DEBUG ad preprocessor options... also made all the stuff work :)

--- rx/src/rx.ino
+++ rx/src/rx.ino
@@ -1,18 +1,25 @@
 #include <VirtualWire.h>
 #include <Enerlib.h>
 
-#define BTN_TIMEOUT_MS 2500
+//#define DEBUG
+#define BEEP
+
+#define DEBOUNCE_MS 200
 //#define SLEEPMODE() energy.Idle();
-#define SLEEPMODE() energy.PowerDown(); //the lowest power setting.
+//#define SLEEPMODE() energy.PowerDown(); //the lowest power setting.
 
 
-Energy energy; //instantiate the low power library
+//Energy energy; //instantiate the low power library
 
 //IO defines:
 int btn_pin = 3;
-int pwm_led = 4;
+int pwm_led = 5;
 int blink_led = 13;
 int RX_pin = 2;
+
+#ifdef BEEP
+int beep_pin = 10;
+#endif
 
 //Global vars:
 
@@ -23,12 +30,10 @@ uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
 //other stuff:
 bool reset = true;
-int chosen_channel=0;
+bool state_handled=false;
+unsigned int chosen_channel=0;
+unsigned int state=0;
 
-bool btn_triggered = false;
-bool trigger_handled = false;
-bool btn_debounced = true;
-unsigned long btn_timer=0;
 
 String msg_gr_1 =   "GR1";
 String msg_gr_2 =   "GR2";
@@ -39,96 +44,113 @@ String msg_gr_6 =   "GR6";
 String msg_gr_7 =   "GR7";
 String msg_gr_8 =   "GR8";
 String msg_on =     "ON!";
+String msg_blink =  "BLK";
 String msg_off =    "OFF";
 String msg_all =    "ALL";
 
-void INT1_ISR(void)
+void INT0_ISR(void)
 {
-  /*
-  The WasSleeping function will return true if Arduino
-  was sleeping before the IRQ. Subsequent calls to
-  WasSleeping will return false until Arduino reenters
-  in a low power state. The WasSleeping function should
-  only be called in the ISR.
-  */
-
-  /*
-    If Arduino was waked up by IRQ.
-    If you shut down external peripherals before sleeping, you
-    can reinitialize them here. Look on ATMega's datasheet for
-    hardware limitations in the ISR when microcontroller just
-    leave any low power state.
-    */
-
-  if (energy.WasSleeping()) reset=true;
-  else btn_triggered=true; //The IRQ happened in awake state.
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > DEBOUNCE_MS)
+  {
+    #ifdef BEEP
+    beep();
+    #endif
+    state=0;
+    state_handled=false;
+    reset=true;
   }
+  last_interrupt_time = interrupt_time;
+}
 
 
 
 void setup() {
 
 
+    #ifdef DEBUG
+    Serial.begin(9600);
+    #endif
 
-    //Serial.begin(9600);
+    #ifdef BEEP
+    pinMode(beep_pin,OUTPUT);
+    #endif
 
     pinMode(pwm_led, OUTPUT);
 
    // Initialise the IO and ISR
     vw_set_rx_pin(RX_pin);
     vw_setup(2000);  // Bits per sec
-//    vw_rx_start();       // Start the receiver PLL running
+    vw_rx_start();       // Start the receiver PLL running
 
     pulse(1,1); //hello world
 
     pinMode(btn_pin,INPUT_PULLUP);
 
-    attachInterrupt(1, INT1_ISR, FALLING);
+    attachInterrupt(0, INT0_ISR, FALLING);
 
-    /*
-  Pin 3 will be the "wake button". Due to uC limitations,
-  it needs to be a level interrupt.
-  For experienced programmers:
-    ATMega's datasheet contains information about the rest of
-    wake up sources. The Extended Standby is not implemented.
-    */
-
-   //energy.Idle();
-   //energy.PowerDown(); //the lowest power setting.
-    SLEEPMODE();
-    //go_to_sleep();
+//    SLEEPMODE();
 
 }
 
 void loop()
     {
-    //for RX'ing:
-    if (reset) vw_rx_start();
 
-    /*Button debouncing*/
-    if (btn_triggered) //<-set by ISR1
-    {
-        btn_timer=millis();
-        btn_triggered = false;
-    }
-    if(!digitalRead(btn_pin) && btn_timer+BTN_TIMEOUT_MS>millis()) btn_debounced=true;
-    else btn_debounced=false;
-    /*end of debounce*/
-
-    if(btn_debounced) go_to_sleep();
-
+            //RX:
     if (vw_get_message(buf, &buflen)) //check for message
         {
+
+            #ifdef DEBUG
+            String rx_string;
+            for (int i = 0; i < buflen; i++) rx_string+=String(char(buf[i]));
+            Serial.print("RX raw: ");
+            Serial.println(rx_string);
+            Serial.print("RX string group: ");
+            Serial.println(group_string_to_int());
+            Serial.print("RX string state: ");
+            Serial.println(rx_to_state());
+            #endif
+
             if (reset) /*if newly awaken, we're armed and ready for a channel designation:*/
                 {
                 reset=false; //disarm
-                /*if (rx_to_state())*/ chosen_channel=group_string_to_int(); //designate channel
-                if (chosen_channel==11) reset=true; //error? REARM!
+                chosen_channel=group_string_to_int(); //designate channel
+                if (chosen_channel>8) reset=true; //error? REARM!
                 }
 
-            else if(chosen_channel==group_string_to_int()) digitalWrite(pwm_led,rx_to_state());
-        }
 
+            else if(group_string_to_int()==chosen_channel || group_string_to_int()==10) //10 = all
+                {
+                    unsigned int newstate=rx_to_state();
+                    if(newstate!=state)
+                        {
+                            state=newstate;
+                            state_handled=false;
+                            #ifdef BEEP
+                            if(state) beep();
+                            #endif
+                        }
+                }
+        }
+            //STATEMACHINE:
+
+    if (!state_handled) switch (state)
+        {
+            case 0:
+                digitalWrite(pwm_led,0);
+                state_handled=true;
+                break;
+            case 1:
+                for (int val=0; val<255; val++) {analogWrite(pwm_led, val); delay(1);}
+                state_handled=true;
+                break;
+            case 2:
+                pulse(1,1);
+                //delay?
+                break;
+        }
 
     }
 
@@ -137,6 +159,7 @@ int group_string_to_int()
 {
     String group_string="";
     for (int i = 0; i < 3; i++) group_string+=String(char(buf[i]));
+
     if     (group_string==msg_gr_1) return 1;
     else if(group_string==msg_gr_2) return 2;
     else if(group_string==msg_gr_3) return 3;
@@ -149,21 +172,60 @@ int group_string_to_int()
     else return 11; //in case of error
 }
 
-bool rx_to_state()
+unsigned int rx_to_state()
 {
-    String onoff_string="";
-    for(int i=3; i<6;i++) onoff_string+=String(char(buf[i]));
-    if(onoff_string=="ON!") return true;
-    return false; //an else would be redundant
-}
+    String rx="";
+    for(int i=3; i<6;i++) rx+=String(char(buf[i]));
+
+    if(rx==msg_on) return 1;
+    if(rx==msg_blink) return 2; //an else would be redundant
+    return 0;
 
 
-void go_to_sleep()
-{
-    chosen_channel=0; //release channel
-    vw_rx_stop();
-    SLEEPMODE();
 }
+
+#ifdef BEEP
+void beep()
+{
+    static unsigned long tone1_reps=300;
+    static unsigned long tone1_delay=125;
+
+    static unsigned long tone2_reps=300;
+    static unsigned long tone2_delay=105;
+
+    static unsigned long tone3_reps=600;
+    static unsigned long tone3_delay=99;
+
+
+    for(unsigned long i=0;i<tone1_reps;i++)
+    {
+        digitalWrite(beep_pin,HIGH);
+        delayMicroseconds(tone1_delay);
+        digitalWrite(beep_pin,LOW);
+        delayMicroseconds(tone1_delay);
+    }
+
+    for(unsigned long i=0;i<tone2_reps;i++)
+    {
+        digitalWrite(beep_pin,HIGH);
+        delayMicroseconds(tone2_delay);
+        digitalWrite(beep_pin,LOW);
+        delayMicroseconds(tone2_delay);
+    }
+
+
+    for(unsigned long i=0;i<tone3_reps;i++)
+    {
+        digitalWrite(beep_pin,HIGH);
+        delayMicroseconds(tone3_delay);
+        digitalWrite(beep_pin,LOW);
+        delayMicroseconds(tone3_delay);
+    }
+
+
+}
+
+#endif
 
 void pulse(unsigned int pulses, unsigned int delay_time)
 {
@@ -175,4 +237,5 @@ void pulse(unsigned int pulses, unsigned int delay_time)
         pulses--;
     }
 }
+
 
